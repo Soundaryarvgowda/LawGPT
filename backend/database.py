b08689938aa0c4ffe70de421f7c3765d9aebbf7b
samrudm <samrudmohan@gmail.com>
2026-05-18 18:11:27 +0530

    Fresh Commit

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://samrud:<db_password>@cluster0.ci4dwri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGODB_URI = os.getenv("MONGODB_URI")
 client = AsyncIOMotorClient(MONGODB_URI)
 db = client.lawgpt
 
